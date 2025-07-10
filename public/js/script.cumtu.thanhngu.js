8e5e7311930f6db5197de0093d382140690dc555
BA2k6 <buian2k6@gmail.com>
2025-07-10 20:56:59 +0700

    Update script.cumtu.thanhngu.js

--- public/js/script.cumtu.thanhngu.js
+++ public/js/script.cumtu.thanhngu.js
@@ -1,12 +1,9 @@
-
 const searchInput = document.getElementById("searchInput");
-
 searchInput.addEventListener("keyup", function () {
-  const filter = this.value.toLowerCase();
+  const filter = this.value.toLowerCase();    //kí tự gõ => chữ thường
   const rows = document.querySelectorAll(".table-wrapper tbody tr");
-
   rows.forEach(row => {
-    const text = row.textContent.toLowerCase();
-    row.style.display = text.includes(filter) ? "" : "none";
+    const text = row.textContent.toLowerCase();          
+    row.style.display = text.includes(filter) ? "" : "none";      // nếu kí tự gõ có trong tr thì gán giá trị "none" cho display của phần tử <tr> - inline-style 
   });
 });
